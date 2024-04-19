8f8be8dcd552bc3ed2852268b74c2521825910fb
Mateusz Somysz <mateusz.somysz@btcx.com>
2024-04-19 10:18:33 +0200

    first commit

---
+++ src/button.tsx
@@ -0,0 +1,17 @@
+import {useState} from "react";
+
+export default function Button() {
+    const [number1, setNumber1] = useState(0);
+    const [number2, setNumber2] = useState(0);
+    return <div>
+        <div>
+            Enter number 1 <input onInput={e => setNumber1(parseInt((e.target as any).value))}/>
+        </div>
+        <div>
+            Enter number 2 <input onInput={e => setNumber2(parseInt((e.target as any).value))}/>
+        </div>
+        <div>
+            Result: {number1 + number2 + 1}
+        </div>
+    </div>;
+}
